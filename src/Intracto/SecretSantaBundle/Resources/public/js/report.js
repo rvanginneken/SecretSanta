73bd8628cc4e98145eeed52c8e1bd6cf79eaf939
Roy Van Ginneken <roy.vanginneken@intracto.com>
2017-09-05 23:50:13 +0200

    SANTA-68: load async

--- src/Intracto/SecretSantaBundle/Resources/public/js/report.js
+++ src/Intracto/SecretSantaBundle/Resources/public/js/report.js
@@ -2,28 +2,29 @@ google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
-    new google.visualization.ColumnChart(document.getElementById('parties_per_year'))
-        .draw(
-            new google.visualization.DataTable($.ajax({
-                url: '/app_dev.php/report/parties_per_year',
-                dataType: "json",
-                async: false
-            }).responseText),
-            {
-                title: 'Total parties created',
-                legend: { position: 'none' }
-            });
+    $.ajax({
+        url: '/app_dev.php/report/parties_per_year',
+        dataType: "json",
+        async: true,
+        success: function (json) {
+            new google.visualization.ColumnChart(document.getElementById('parties_per_year')).draw(
+                new google.visualization.DataTable(json), {
+                    title: 'Total parties created',
+                    legend: { position: 'none' }
+                });
+        }
+    });
 
-
-    new google.visualization.PieChart(document.getElementById('ip_type_visitors'))
-        .draw(
-            new google.visualization.DataTable($.ajax({
-                url: '/app_dev.php/report/ip_type_visitors',
-                dataType: "json",
-                async: false
-            }).responseText),
-            {
-                title: 'Ip type of visitors',
-                legend: { position: 'none' }
-            });
+    $.ajax({
+        url: '/app_dev.php/report/ip_type_visitors',
+        dataType: "json",
+        async: true,
+        success: function (json) {
+            new google.visualization.PieChart(document.getElementById('ip_type_visitors')).draw(
+                new google.visualization.DataTable(json), {
+                    title: 'Ip type of visitors',
+                    legend: { position: 'none' }
+                });
+        }
+    });
 }
